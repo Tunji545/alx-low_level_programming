31c9f78c70a1e605500d1f4188e5c8d44c40433b
Tunji545 <msknolatunji@gmail.com>
2022-07-02 14:33:53 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+ *
+ *main -> Write a program that prints the
+ *
+ *alphabet in lowercase,
+ *
+ *Return: Always O (success)
+ *
+ **/
+
+
+
+
+int main(void)
+
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	putchar(10);
+	return (0);
+
+
+
+}
