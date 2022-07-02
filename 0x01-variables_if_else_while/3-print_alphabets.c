f1b6b2d5f86b4653f07cf36901c3833f218a1e92
Tunji545 <msknolatunji@gmail.com>
2022-07-02 16:48:40 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  *main -> assign a random number
+ *
+ *   *and print the last digit of the
+ *
+ *    *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+
+{
+	int ch;
+		for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
+
+
