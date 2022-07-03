a3009ae578de562f84a18640750a38a8a8331e06
Tunji545 <msknolatunji@gmail.com>
2022-07-03 13:39:23 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+/**
+ *  *main -> Write a program that prints all the numbers
+ *
+ *   *of base 16 in lowercase, followed by a new line.
+ *
+ *    *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+
+{
+
+	char ch;
+	int n;
+		for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
+
+
