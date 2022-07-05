07bbfe40e4c05f55a64a36cca57a50916afde6ff
Tunji545 <msknolatunji@gmail.com>
2022-07-04 17:08:50 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  *main -> assign a random number
+ *
+ *   *and print the last digit of the
+ *
+ *    *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
