a088b54245238761c253961a64860ee6e1dda5b4
Tunji545 <msknolatunji@gmail.com>
2022-07-10 22:48:00 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*  *_isalpha - checks for alphabetic characters
+*
+*   *@c: integer
+*
+*    *Return: 1
+*/
+int _isalpha(int c)
+{
+	int x;
+
+	for ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+	{
+		if (x == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
