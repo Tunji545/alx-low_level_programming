494d34fe7eb549bce9606f57e8eb6f730752da07
Tunji545 <msknolatunji@gmail.com>
2022-07-11 15:20:09 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+*  *_abs - computes the absolute
+*
+*   *value of an integer
+*
+*    *@n: integer
+*
+*     *Return: 0
+*/
+int _abs(int n)
+{
+	int x;
+
+	x = abs(n);
+	_putchar(x);
+	return (0);
+}
