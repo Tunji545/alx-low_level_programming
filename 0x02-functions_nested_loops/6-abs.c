5badc71144b794385e40a6ed46e640d460569f86
Tunji545 <msknolatunji@gmail.com>
2022-07-12 00:04:02 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include <limits.h>
 /**
 *  *_abs - computes the absolute
 *
@@ -10,10 +11,11 @@
 *
 *     *Return: 0
 */
-int _abs(int)
+int _abs(int n)
 {
 	unsigned int x;
-	int const mask = n >> sizeof(int) * CHAR_BIT - 1;
+	int const mask;
+		mask = n >> sizeof(int) * CHAR_BIT - 1;
 	putchar(mask);
 	putchar('\n');
 	return (0);
