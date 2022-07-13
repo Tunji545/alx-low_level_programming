c55f7495b3b8294384a647893e5d5e89b8c66ae4
Tunji545 <msknolatunji@gmail.com>
2022-07-13 12:50:25 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*  *print_to_98 - Function that prints from
+*
+*   *n - 98 natural numbers
+*
+*    *@n: integer
+*
+*     *Return : 0
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for(n = 0; m <= 98; n++)
+	{
+		_printf(n);
+		_printf(' ');
+	}
+	return (0);
+}
